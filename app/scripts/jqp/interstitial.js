df8448234a4aa477ca6e23035ded58cabadd802f
Jose Antonio Guerra <jaguerra@icti.es>
2015-08-20 13:55:59 +0200

    [WIP] JS Interstitial, Lightbox refactor

---
+++ app/scripts/jqp/interstitial.js
@@ -0,0 +1,64 @@
+define(['jquery', 'jqp/cookie'],
+       function (jQuery) {
+
+         /**
+          * Opens an external URL into a Lightbox.
+          * Lightbox will only be triggered on the following constraints:
+          * - "open" action is called
+          * - it is the first page visit on the current session
+          *
+          * Usage:
+          *
+          * "init" action should be called on every page hit. "open" action should only be called on pages suitable for the
+          * lightbox to open.
+          *
+          */
+         jQuery.interstitial = function( action, options ) {
+
+           var settings = jQuery.extend({}, jQuery.interstitial.defaults, options );
+
+           if( action === 'init' || typeof action === 'undefined' ){
+
+             setVisitCookie(settings);
+
+           } else if(action === 'open'){
+
+             if( isFirstVisit(settings) ){
+               openLightbox(settings);
+               setVisitCookie(settings);
+             }
+           }
+
+           return this;
+
+         };
+
+         jQuery.interstitial.defaults = {
+           cookieName: "interstitial",
+           url: "",
+           title: "",
+           width: "750",
+           height: "400"
+         };
+
+         function setVisitCookie(settings){
+           jQuery.cookie( settings.cookieName, '1', { path: '/' });
+         }
+
+         function isFirstVisit(settings){
+           if( jQuery.cookie( settings.cookieName ) === '1'){
+             return false;
+           } else {
+             return true;
+           }
+         }
+
+         function openLightbox(settings){
+           require(['domReady', 'lightbox'], function(domReady, lightbox) {
+             domReady(function(){
+               lightbox.open_iframe(settings.url);
+             });
+           });
+         }
+
+       });
--- app/scripts/lightbox.js
+++ app/scripts/lightbox.js
@@ -1,11 +1,42 @@
 define(['jquery', 'magnific-popup'], function($){
-  $(document).ready(function() {
 
-    var galleries_ids = [],
-      galleries_visited = {},
-      $lightbox_elements = $('[rel^=lightbox'),
-      $lightbox_elements_html = $('.ai-lightbox--html'),
-      i = 0;
+  var _lightbox = (function() {
+
+    var base_options_iframe = {
+      mainClass: 'ai-lightbox-popup ai-lightbox-popup--iframe',
+      type: 'iframe'
+    };
+
+    var base_options_image = {
+      type: 'image',
+      mainClass: 'ai-lightbox-popup ai-lightbox-popup--image',
+      closeOnContentClick: true,
+      image: {
+        verticalFit: true
+      }
+    };
+
+    var base_options_image_gallery = {
+      type: 'image',
+      mainClass: 'ai-lightbox-popup ai-lightbox-popup--image ai-lightbox-popup--image-gallery',
+      closeOnContentClick: true,
+      image: {
+        verticalFit: true
+      },
+      gallery: {
+        enabled: true
+      }
+    };
+
+    function open_iframe(url) {
+      var options = {
+        items: {
+          src: url
+        }
+      };
+      $.extend( true, options, base_options_iframe);
+      $.magnificPopup.open(options);
+    }
 
     function get_gallery_id_from_rel(rel) {
       var rel_match = rel.match(/lightbox\[(.+)\]/);
@@ -16,48 +47,58 @@ define(['jquery', 'magnific-popup'], function($){
       }
     }
 
-    $lightbox_elements.each(function() {
-      var rel = $( this ).attr('rel'),
-        gallery = get_gallery_id_from_rel(rel);
+    function init_data_api() {
 
-      if (gallery !== false) {
-        if (galleries_visited[gallery] === undefined) {
-          galleries_visited[gallery] = true;
-          galleries_ids.push( gallery );
-        }
-        $(this).data('lightbox_gallery_id', gallery);
-      }
-    });
+      var galleries_ids = [],
+        galleries_visited = {},
+        $lightbox_elements = $('[rel^=lightbox'),
+        $lightbox_elements_html = $('.ai-lightbox--html'),
+        i = 0;
 
-    for(i=0;i < galleries_ids.length;i++) {
-      $lightbox_elements.filter( function() {
+      function filter_gallery_id () {
         return $(this).data('lightbox_gallery_id') === galleries_ids[i];
-      }).magnificPopup({
-        type: 'image',
-        closeOnContentClick: true,
-        image: {
-          verticalFit: true
-        },
-        gallery: {
-          enabled: true
+      }
+
+
+      // DOM elements with rel="lightbox[gallery_id]"
+      // Each gallery_id stands for an indepentent gallery
+      $lightbox_elements.each(function() {
+        var rel = $( this ).attr('rel'),
+          gallery = get_gallery_id_from_rel(rel);
+
+        if (gallery !== false) {
+          if (galleries_visited[gallery] === undefined) {
+            galleries_visited[gallery] = true;
+            galleries_ids.push( gallery );
+          }
+          $(this).data('lightbox_gallery_id', gallery);
         }
       });
+
+      for(i=0;i < galleries_ids.length;i++) {
+        $lightbox_elements.filter(filter_gallery_id).magnificPopup(base_options_image_gallery);
+      }
+
+      // DOM elements with rel="lightbox"
+      $lightbox_elements.filter( function() {
+        return $(this).data('lightbox_gallery_id') === undefined;
+      }).magnificPopup(base_options_image);
+
+      // DOM elements with class "ai-lightbox--html" get an iframe lightbox
+      $lightbox_elements_html.magnificPopup(base_options_iframe);
     }
 
-    $lightbox_elements.filter( function() {
-      return $(this).data('lightbox_gallery_id') === undefined;
-    }).magnificPopup({
-      type: 'image',
-      closeOnContentClick: true,
-      image: {
-        verticalFit: true
-      }
-    });
+    return {
+      'init_data_api': init_data_api,
+      'open_iframe': open_iframe
+    };
 
-    $lightbox_elements_html.magnificPopup({
-      type: 'iframe'
-    });
+  })();
 
+  $(document).ready(function() {
+    _lightbox.init_data_api();
   });
-});
 
+  return _lightbox;
+
+});
